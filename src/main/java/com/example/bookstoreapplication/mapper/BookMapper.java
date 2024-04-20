f610a17b6f2b1cfa5f8ef86cd7d308c89c0d1281
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-04-20 21:13:40 +0300

    implemented Mapstruct

--- src/main/java/com/example/bookstoreapplication/mapper/BookMapper.java
+++ src/main/java/com/example/bookstoreapplication/mapper/BookMapper.java
@@ -1,9 +1,12 @@
 package com.example.bookstoreapplication.mapper;
 
+import com.example.bookstoreapplication.config.MapperConfig;
 import com.example.bookstoreapplication.dto.BookDto;
 import com.example.bookstoreapplication.dto.CreateBookRequestDto;
 import com.example.bookstoreapplication.model.Book;
+import org.mapstruct.Mapper;
 
+@Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toDto(Book book);
 
