6dbbca81b7462c8ea2423bdc7acd92da90ff0c6d
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-04-20 19:28:22 +0300

    created BookController class

---
+++ src/main/java/com/example/bookstoreapplication/controller/BookController.java
@@ -0,0 +1,20 @@
+package com.example.bookstoreapplication.controller;
+
+import com.example.bookstoreapplication.dto.BookDto;
+import com.example.bookstoreapplication.dto.CreateBookRequestDto;
+
+import java.util.List;
+
+public class BookController {
+    public List<BookDto> getAll() {
+        return null;
+    }
+
+    public BookDto getBookById(long id) {
+        return null;
+    }
+
+    public BookDto createBook(CreateBookRequestDto dto) {
+        return null;
+    }
+}
