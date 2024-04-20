cf01b062cbb3901255ccad64a1bdd5a604f60cdc
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-04-20 22:29:36 +0300

    added unmappedTargetPolicy to BookMapper

--- src/main/java/com/example/bookstoreapplication/mapper/BookMapper.java
+++ src/main/java/com/example/bookstoreapplication/mapper/BookMapper.java
@@ -5,9 +5,11 @@ import com.example.bookstoreapplication.dto.BookDto;
 import com.example.bookstoreapplication.dto.CreateBookRequestDto;
 import com.example.bookstoreapplication.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(config = MapperConfig.class)
+@Mapper(config = MapperConfig.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface BookMapper {
+
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto dto);
