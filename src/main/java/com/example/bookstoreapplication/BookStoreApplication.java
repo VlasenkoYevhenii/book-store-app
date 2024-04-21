42d971d22f23bde77f807cddb406ae79af975996
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-04-21 16:42:46 +0300

    resolved comments

--- src/main/java/com/example/bookstoreapplication/BookStoreApplication.java
+++ src/main/java/com/example/bookstoreapplication/BookStoreApplication.java
@@ -1,7 +1,5 @@
 package com.example.bookstoreapplication;
 
-import com.example.bookstoreapplication.mapper.BookMapper;
-import com.example.bookstoreapplication.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @RequiredArgsConstructor
 public class BookStoreApplication {
-    private final BookService bookService;
-    private final BookMapper mapper;
-
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
