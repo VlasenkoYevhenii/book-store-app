7cd12cdfe2c89e006eed7def745e7cdd5d624e23
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-04-29 21:21:26 +0300

    changed the annotation over price field in CreateBookRequestDto to @Positive

--- src/main/java/com/example/bookstoreapplication/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/bookstoreapplication/dto/CreateBookRequestDto.java
@@ -1,9 +1,9 @@
 package com.example.bookstoreapplication.dto;
 
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
 
@@ -20,7 +20,7 @@ public class CreateBookRequestDto {
     private String isbn;
 
     @NotNull(message = "Define the book price")
-    @DecimalMin(value = "0.01", message = "The price must be greater than 0")
+    @Positive(message = "The price must be greater than 0")
     private BigDecimal price;
 
     private String description;
