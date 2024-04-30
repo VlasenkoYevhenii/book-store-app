116e11944a2554e084b62d7770f9fcba9b4c3693
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-04-30 11:13:05 +0300

    added Validated annotation over the BookControllerClass

--- src/main/java/com/example/bookstoreapplication/controller/BookController.java
+++ src/main/java/com/example/bookstoreapplication/controller/BookController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/books")
 @RequiredArgsConstructor
+@Validated
 public class BookController {
     private final BookService bookService;
 
