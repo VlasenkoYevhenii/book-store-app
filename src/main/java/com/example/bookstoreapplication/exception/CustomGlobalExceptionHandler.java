6abcb93842818e6e0657456ca0086ca2d1c8c95d
VlasenkoYevhenii <159646520+VlasenkoYevhenii@users.noreply.github.com>
2024-05-02 12:43:23 +0300

    Global exception handler (#5)
    * changed the annotation over price field in CreateBookRequestDto to @Positive
    
    * added Validated annotation over the BookControllerClass
    
    * implemented CustomGlobalEceptionHandler
    
    * resolved comments
    
    ---------
    
    Co-authored-by: Vlasenko Yevhenii <vlasenkoevy@gmail.com>

---
+++ src/main/java/com/example/bookstoreapplication/exception/CustomGlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package com.example.bookstoreapplication.exception;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final String TIMESTAMP = "timestamp";
+    private static final String STATUS = "status";
+    private static final String ERROR = "error";
+    private static final String MESSAGE = "message";
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put(TIMESTAMP, LocalDateTime.now());
+        body.put(STATUS, HttpStatus.BAD_REQUEST);
+        List<String> errors = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(this::getErrorMessage).toList();
+        body.put(ERROR, errors);
+        return new ResponseEntity<>(body, headers, status);
+    }
+
+    @ExceptionHandler({EntityNotFoundException.class})
+    protected ResponseEntity<Object> handleEntityNotFoundException(Exception ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put(TIMESTAMP, LocalDateTime.now());
+        body.put(STATUS, HttpStatus.NOT_FOUND);
+        body.put(MESSAGE, ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({DataProcessingException.class})
+    protected ResponseEntity<Object> handleDataProcessingException(Exception ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put(TIMESTAMP, LocalDateTime.now());
+        body.put(STATUS, HttpStatus.INTERNAL_SERVER_ERROR);
+        body.put(MESSAGE, ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private String getErrorMessage(ObjectError object) {
+        if (object instanceof FieldError) {
+            String field = ((FieldError) object).getField();
+            String message = object.getDefaultMessage();
+            return field + " " + message;
+        }
+        return object.getDefaultMessage();
+    }
+}
