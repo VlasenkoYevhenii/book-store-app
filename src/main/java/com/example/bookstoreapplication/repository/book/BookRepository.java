c8235d9b5e0d96feb5dfad4062827974379c6bbc
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-13 16:16:31 +0300

    created BookDtoWithoutCategoryIds class

--- src/main/java/com/example/bookstoreapplication/repository/book/BookRepository.java
+++ src/main/java/com/example/bookstoreapplication/repository/book/BookRepository.java
@@ -1,9 +1,14 @@
 package com.example.bookstoreapplication.repository.book;
 
 import com.example.bookstoreapplication.model.Book;
+import com.example.bookstoreapplication.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
-
+    List<Book> findByCategoriesId(Long id);
 }
