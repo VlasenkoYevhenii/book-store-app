102140453feeec92f51749c42cc98236ffe6399a
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-13 16:29:21 +0300

    added categoriesIds field to BookDto

--- src/main/java/com/example/bookstoreapplication/dto/book/BookDto.java
+++ src/main/java/com/example/bookstoreapplication/dto/book/BookDto.java
@@ -1,6 +1,8 @@
 package com.example.bookstoreapplication.dto.book;
 
 import java.math.BigDecimal;
+import java.util.Set;
+
 import lombok.Data;
 
 @Data
@@ -12,4 +14,5 @@ public class BookDto {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    private Set<Long> categoriesIds;
 }
