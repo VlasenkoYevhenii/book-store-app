fbf6204d5ede259ff05f331547cfe40ac7ad99dc
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-13 18:07:29 +0300

    soft delete for category entity

--- src/main/java/com/example/bookstoreapplication/model/Category.java
+++ src/main/java/com/example/bookstoreapplication/model/Category.java
@@ -1,19 +1,28 @@
 package com.example.bookstoreapplication.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @Setter
+@SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id=?")
+@SQLRestriction("is_deleted = false")
+@Table(name = "categories")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
+    @Column(nullable = false)
+    private boolean isDeleted;
 }
