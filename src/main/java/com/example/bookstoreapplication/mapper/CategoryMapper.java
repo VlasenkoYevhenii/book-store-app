168d4528bad0cd0e547b85b919fd9b27a2e67256
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-13 16:46:35 +0300

    created CategoryMapper

---
+++ src/main/java/com/example/bookstoreapplication/mapper/CategoryMapper.java
@@ -0,0 +1,12 @@
+package com.example.bookstoreapplication.mapper;
+
+import com.example.bookstoreapplication.config.MapperConfig;
+import com.example.bookstoreapplication.model.Category;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapperConfig.class)
+public interface CategoryMapper {
+    CategoryDto toDto(Category category);
+
+    Category toModel(CategoryDto dto);
+}
