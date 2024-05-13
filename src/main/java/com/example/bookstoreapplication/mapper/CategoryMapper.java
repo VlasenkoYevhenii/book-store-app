2297114d21cab4acf5593426317484923e7d7d3d
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-13 18:14:51 +0300

    mapper fix

--- src/main/java/com/example/bookstoreapplication/mapper/CategoryMapper.java
+++ src/main/java/com/example/bookstoreapplication/mapper/CategoryMapper.java
@@ -4,8 +4,9 @@ import com.example.bookstoreapplication.config.MapperConfig;
 import com.example.bookstoreapplication.dto.category.CategoryDto;
 import com.example.bookstoreapplication.model.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(config = MapperConfig.class)
+@Mapper(config = MapperConfig.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CategoryMapper {
     CategoryDto toDto(Category category);
 
