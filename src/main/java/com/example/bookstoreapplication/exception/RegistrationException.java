272a0251bd85c540a09efed1a2a87a40e08168cd
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-14 11:31:10 +0300

    resolved comments

--- src/main/java/com/example/bookstoreapplication/exception/RegistrationException.java
+++ src/main/java/com/example/bookstoreapplication/exception/RegistrationException.java
@@ -3,4 +3,8 @@ package com.example.bookstoreapplication.exception;
 public class RegistrationException extends Exception {
     public RegistrationException(String message) {
     }
+
+    public RegistrationException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
