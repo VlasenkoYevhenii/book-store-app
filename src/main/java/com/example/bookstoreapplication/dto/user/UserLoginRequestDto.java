f853be3c22a54ca37e0ad08190469ddfd9faa820
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-14 16:01:39 +0300

    resolved new comments

--- src/main/java/com/example/bookstoreapplication/dto/user/UserLoginRequestDto.java
+++ src/main/java/com/example/bookstoreapplication/dto/user/UserLoginRequestDto.java
@@ -2,6 +2,7 @@ package com.example.bookstoreapplication.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
@@ -9,6 +10,7 @@ import org.hibernate.validator.constraints.Length;
 public class UserLoginRequestDto {
     @NotBlank(message = "The email field is empty")
     @Email(message = "The email format is invalid")
+    @Size(min = 4, max = 40)
     private String email;
 
     @NotBlank(message = "The password field is empty")
