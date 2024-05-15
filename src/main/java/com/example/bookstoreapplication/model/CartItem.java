7d1c2389085afa3d304db4963d89fddd32d86d1f
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-15 12:20:48 +0300

    added entities relations

--- src/main/java/com/example/bookstoreapplication/model/CartItem.java
+++ src/main/java/com/example/bookstoreapplication/model/CartItem.java
@@ -1,24 +1,33 @@
 package com.example.bookstoreapplication.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "cart_items")
 public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shopping_cart_id")
     private ShoppingCart shoppingCart;
+
     @OneToOne
+    @JoinColumn(name = "book_id", nullable = false)
     private Book book;
+
     private int quantity;
 }
--- src/main/java/com/example/bookstoreapplication/model/ShoppingCart.java
+++ src/main/java/com/example/bookstoreapplication/model/ShoppingCart.java
@@ -1,6 +1,8 @@
 package com.example.bookstoreapplication.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -8,24 +10,31 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Data
+@Getter
+@Setter
 @SQLDelete(sql = "UPDATE carts SET is_deleted = true WHERE id=?")
 @SQLRestriction("is_deleted = false")
-@Table(name = "carts")
+@Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @OneToOne
     private User user;
-    @OneToMany
-    private Set<CartItem> cartItems;
 
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "shoppingCart")
+    private Set<CartItem> cartItems = new HashSet<>();
+
+    @Column(nullable = false)
     private boolean isDeleted;
 }
