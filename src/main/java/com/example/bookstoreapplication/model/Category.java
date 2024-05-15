cc1f9c6198f475b0210e3c97b863454ec02c253b
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-15 12:20:26 +0300

    added nullable=false for "name" field

--- src/main/java/com/example/bookstoreapplication/model/Category.java
+++ src/main/java/com/example/bookstoreapplication/model/Category.java
@@ -21,6 +21,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
     private String description;
     @Column(nullable = false)
