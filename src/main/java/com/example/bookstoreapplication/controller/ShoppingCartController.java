aef5ec72f3ed41d11dd102873d6c0ba438fcd6fb
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-16 17:38:14 +0300

    swagger and preauthorize

--- src/main/java/com/example/bookstoreapplication/controller/ShoppingCartController.java
+++ src/main/java/com/example/bookstoreapplication/controller/ShoppingCartController.java
@@ -5,7 +5,11 @@ import com.example.bookstoreapplication.dto.cartitem.CartItemUpdateDto;
 import com.example.bookstoreapplication.dto.shoppingcart.ShoppingCartResponseDto;
 import com.example.bookstoreapplication.model.User;
 import com.example.bookstoreapplication.service.shoppingcart.ShoppingCartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,21 +18,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/cart")
+@Tag(name = "Shopping Cart", description = "Shopping Cart management")
 public class ShoppingCartController {
     private final ShoppingCartService shoppingCartService;
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping
+    @Operation(summary = "Get a shoppingCart",
+            description = "Endpoint for getting user's shoppingCart from the db")
+    @ResponseStatus(HttpStatus.OK)
     public ShoppingCartResponseDto getShoppingCart(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         return shoppingCartService.getByUserId(user.getId());
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping
+    @Operation(summary = "Add a cartItem to the shopping cart",
+            description = "Endpoint for adding a cartItem to the shopping cart")
+    @ResponseStatus(HttpStatus.CREATED)
     public ShoppingCartResponseDto addBookToShoppingCart(Authentication authentication,
                                                  @RequestBody CartItemRequestDto cartItemRequestDto
                                                          ) {
@@ -36,7 +50,11 @@ public class ShoppingCartController {
         return shoppingCartService.addBookToCart(user.getId(), cartItemRequestDto);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @PutMapping("/cart-items/{cartItemId}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Update books quantity",
+            description = "Endpoint for updating books quantity in the db")
     public ShoppingCartResponseDto updateBookQuantity(@PathVariable Long cartItemId,
                                                       Authentication authentication,
                                                       CartItemUpdateDto updateDto) {
@@ -44,7 +62,11 @@ public class ShoppingCartController {
         return shoppingCartService.updateBookQuantityById(cartItemId, user.getId(), updateDto);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @DeleteMapping("/cart-items/{cartItemId}")
+    @Operation(summary = "Delete a cartItem",
+            description = "Endpoint for deleting a cartItem from the db")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeBookFromCart(@PathVariable Long cartItemId, Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         shoppingCartService.removeBookFromCart(user.getId(), cartItemId);
