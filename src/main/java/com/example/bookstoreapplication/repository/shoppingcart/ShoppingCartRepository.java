9d2901a0aec22584e1b326d020564af8eef0d828
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-16 11:44:50 +0300

    ShoppingCartRepository

--- src/main/java/com/example/bookstoreapplication/repository/shoppingcart/ShoppingCartRepository.java
+++ src/main/java/com/example/bookstoreapplication/repository/shoppingcart/ShoppingCartRepository.java
@@ -4,4 +4,5 @@ import com.example.bookstoreapplication.model.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+    ShoppingCart findShoppingCartByUserId(Long userId);
 }
