f377c69805dc39f6eae3f4f88e7d3f011e49bfcb
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-16 11:43:13 +0300

    ShoppingCartService

---
+++ src/main/java/com/example/bookstoreapplication/service/shoppingcart/ShoppingCartService.java
@@ -0,0 +1,15 @@
+package com.example.bookstoreapplication.service.shoppingcart;
+
+import com.example.bookstoreapplication.dto.cartitem.CartItemRequestDto;
+import com.example.bookstoreapplication.dto.cartitem.CartItemUpdateDto;
+import com.example.bookstoreapplication.dto.shoppingcart.ShoppingCartResponseDto;
+
+public interface ShoppingCartService {
+    ShoppingCartResponseDto getByUserId(Long userId);
+
+    ShoppingCartResponseDto addBookToCart(Long userId, CartItemRequestDto requestDto);
+
+    ShoppingCartResponseDto updateBookQuantityById(Long cartItemId, Long userId, CartItemUpdateDto updateDto);
+
+    void removeBookFromCart(Long userId, Long cartItemId);
+}
