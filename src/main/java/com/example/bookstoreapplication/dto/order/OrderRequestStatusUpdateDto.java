2a0634d75f4ce5e3d69d9ee4042ad1fddc796a5f
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-19 18:37:29 +0300

    added OrderRequestStatusUpdateDto

---
+++ src/main/java/com/example/bookstoreapplication/dto/order/OrderRequestStatusUpdateDto.java
@@ -0,0 +1,10 @@
+package com.example.bookstoreapplication.dto.order;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderRequestStatusUpdateDto {
+    private String status;
+}
