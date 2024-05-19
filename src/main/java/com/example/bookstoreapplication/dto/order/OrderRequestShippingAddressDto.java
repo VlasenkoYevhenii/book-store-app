0a318f6bffd09cdc1d27990de4c0af59160f64ec
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-19 18:38:42 +0300

    renamed OrderShippingAddressRequestDto to OrderRequestShippingAddressDto

--- src/main/java/com/example/bookstoreapplication/dto/order/OrderRequestShippingAddressDto.java
+++ src/main/java/com/example/bookstoreapplication/dto/order/OrderRequestShippingAddressDto.java
@@ -3,7 +3,7 @@ package com.example.bookstoreapplication.dto.order;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public class OrderShippingAddressRequestDto {
+public class OrderRequestShippingAddressDto {
     @NotBlank
     @Size(min = 10, max = 100)
     private String shippingAddress;
