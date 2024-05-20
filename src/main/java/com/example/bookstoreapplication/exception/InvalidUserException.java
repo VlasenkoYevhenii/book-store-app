ab3f754d7ce6246dc42a6d580b4e8b716654f0b5
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-05-20 07:34:23 +0300

    deleted excess exception

--- src/main/java/com/example/bookstoreapplication/exception/InvalidUserException.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookstoreapplication.exception;
-
-public class InvalidUserException extends RuntimeException {
-    public InvalidUserException(String message) {
-        super(message);
-    }
-}
