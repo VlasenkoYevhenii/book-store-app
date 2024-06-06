14f24013ee7ca37e20f298f490a59482cf8e778b
Yevhenii Vlasenko <yevhenii.vlasenko.work@gmail.com>
2024-06-06 19:21:26 +0300

    fixed lazy on search endpoint

--- src/main/java/com/example/bookstoreapplication/model/Book.java
+++ src/main/java/com/example/bookstoreapplication/model/Book.java
@@ -2,6 +2,7 @@ package com.example.bookstoreapplication.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -38,7 +39,7 @@ public class Book {
     private String coverImage;
     @Column(nullable = false)
     private boolean isDeleted;
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "books_categories",
             joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
